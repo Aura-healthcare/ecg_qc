a7b550f2dd43c73374d29aa84a3b03464578a100
Alexis COMTE <alexis.g.comte@gmail.com>
2021-09-09 12:15:51 +0200

    adding commments to EcgQc class

--- ecg_qc/ecg_qc.py
+++ ecg_qc/ecg_qc.py
@@ -10,7 +10,32 @@ lib_path = os.path.dirname(__file__)
 
 
 class EcgQc:
+    """
+    This class determines the quality of an ECG segment, usually lasting
+    several seconds. It computes SQIs (Signal Quality Indicator) and use them
+    in a pre-trained model to predict the quality:
+        * 1 : good quality
+        * 0 : bad quality
 
+    Attributes
+    ----------
+    model :
+        Trained model to load to predict quality
+    sampling_frequency : int
+        Sampling frequency of the input ECG signal. Used for several SQI
+        computing
+    normalized : bool
+        If True, will normalise input ecg signal
+
+    Methods
+    -------
+    compute_sqi_scores(ecg_signal)
+        Computes SQIs from an ECG signal segment
+    predict_quality(sqi_scores)
+        From a list of SQIs, predict the quality of a related ECG segment
+    get_signal_quality(ecg_signal)
+        From an ECG signal segment, directly returns the quality
+    """
     def __init__(self,
                  model=f'{lib_path}/ml/models/xgb_9s.joblib',
                  sampling_frequency: int = 1000,
@@ -22,11 +47,24 @@ class EcgQc:
 
     def compute_sqi_scores(self,
                            ecg_signal: list) -> list:
+        """
+        From an ECG Signal segment, computes 6 SQI scores (q_sqi, c_sqi, s_sqi,
+        k_sqi, p_sqi, bas_sqi)
 
+        Parameters
+        ----------
+        ecg_signal : list
+            Input ECG signal
+
+        Returns
+        -------
+        sqi_scores : list
+            SQI scores related to input ECG segment
+        """
         if self.normalized:
             ecg_signal = StandardScaler().fit_transform(
                 ecg_signal.reshape(-1, 1)).reshape(1, -1)[0]
-	
+
         q_sqi_score = qsqi(ecg_signal, self.sampling_frequency)
         c_sqi_score = csqi(ecg_signal, self.sampling_frequency)
 
@@ -42,15 +80,42 @@ class EcgQc:
         return sqi_scores
 
     def predict_quality(self, sqi_scores: list) -> int:
- 
+        """
+        From an ECG segment SQI scores, use pre-trained model to compute
+        the quality of the signal.
+
+        Parameters
+        ----------
+        sqi_scores : list(list)
+            SQI scores related to input ECG segment
+
+        Returns
+        -------
+        prediction : int
+            The signal quality predicted by the model
+        """
         sqi_scores = np.array(sqi_scores)
         prediction = int(self.model.predict(sqi_scores))
-	    
+
         return prediction
 
     def get_signal_quality(self,
                            ecg_signal: list) -> int:
+        """
+        From an ECG segment signal, use pre-trained model to compute
+        the quality of the signal. This method is a shortcut to using
+        compute_sqi_scores then predict quality.
 
+        Parameters
+        ----------
+        ecg_signal : list
+            Input ECG signal
+
+        Returns
+        -------
+        prediction : int
+            The signal quality predicted by the model
+        """
         sqi_scores = self.compute_sqi_scores(ecg_signal)
         quality_predicted = self.predict_quality(sqi_scores)
 
