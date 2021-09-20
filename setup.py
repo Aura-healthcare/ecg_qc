c08c335f93a772837f7772003a6ce1f8bd579399
Alexis COMTE <alexis.g.comte@gmail.com>
2021-09-20 18:08:56 +0200

    adding numpy version for compatility with python3.7

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
     install_requires=[
         "biosppy>=0.6.1",
         "dill>=0.3.4",
+        "numpy>=1.19.5",
         "pathtools>=0.1.2",
         "py-ecg-detectors>=1.0.2",
         "scikit-learn>=0.24.2",
